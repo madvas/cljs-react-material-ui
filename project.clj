a8d1a529347c51b7ade5d9435e841ce88a75f25f
madvas <matus.lestan@gmail.com>
2017-02-21 19:55:23 +0100

    Update to material-ui 0.17.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject cljs-react-material-ui "0.2.37"
+(defproject cljs-react-material-ui "0.2.38"
   :description "Iterop library for material-ui.com"
   :url "https://github.com/madvas/cljs-react-material-ui"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.40"]
-                 [cljsjs/material-ui "0.16.7-0"]
+                 [cljsjs/material-ui "0.17.0-0"]
                  [camel-snake-kebab "0.4.0"]])
